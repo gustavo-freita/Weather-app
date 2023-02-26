e2f8b7afab3c9975757faa7532ee32245c58732e
Gustavo Freitas <gustavofreitas_silva@hotmail.com>
2023-02-26 19:06:10 -0300

    connection with the api made

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,10 +1,23 @@
-const content = document.querySelector(".content"),
-inputPart = content.querySelector(".input__part"),
-infoTxt = content.querySelector(".info__text"),
-inputField = content.querySelector("input");
+const apiKey = `767598619d236762077a0a2005f5fd11`;
+const content = document.querySelector(".content");
+const inputPart = content.querySelector(".input__part");
+const infoTxt = inputPart.querySelector(".info__text");
+const inputField = inputPart.querySelector("input");
+const locationBtn = inputPart.querySelector("button");
 
-inputField.addEventListener('keyup', e =>{
+inputField.addEventListener("keyup", e =>{
     if(e.key == "Enter" && inputField.value != ""){
-        console.log("hello")
+        requestApi(inputField.value);
     }
 }) 
+
+function requestApi(city){
+    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    infoTxt.innerText = "Pegando detalhes do tempo...";
+    infoTxt.classList.add("pending");
+    fetch(api).then(Response => Response.json()).then(result => weatherDetail(result));
+}
+
+function weatherDetail(info){
+    console.log(info);
+}
