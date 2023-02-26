4e074791a9c24853d378db63d6dd5b9537d4e9c7
Gustavo Freitas <gustavofreitas_silva@hotmail.com>
2023-02-26 19:45:08 -0300

    search for the place of the person made

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -5,19 +5,50 @@ const infoTxt = inputPart.querySelector(".info__text");
 const inputField = inputPart.querySelector("input");
 const locationBtn = inputPart.querySelector("button");
 
-inputField.addEventListener("keyup", e =>{
+let api;
+
+inputField.addEventListener("keyup", e => {
     if(e.key == "Enter" && inputField.value != ""){
         requestApi(inputField.value);
     }
 }) 
 
-function requestApi(city){
-    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+locationBtn.addEventListener("click", ()=> {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    } else {
+        alert("Seu brower não suporta a API");
+    }
+})
+
+function onSuccess(position) {
+    const {latitude, longitude} = position.coords;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+    fetchData();
+}
+
+function onError(error) {
+    infoTxt.innerHTML = error.message;
+    infoTxt.classList.add("erro");
+}
+
+function requestApi(city) {
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    fetchData();
+}
+
+function fetchData(){
     infoTxt.innerText = "Pegando detalhes do tempo...";
     infoTxt.classList.add("pending");
     fetch(api).then(Response => Response.json()).then(result => weatherDetail(result));
 }
 
-function weatherDetail(info){
-    console.log(info);
+function weatherDetail(info) {
+    infoTxt.classList.replace("pending", "erro");
+    if(info.cod == "404") {
+        infoTxt.innerText = `${inputField.valu} Não é uma cidade valida`;
+    } else {
+        infoTxt.classList.remove("pending", "erro");
+        console.log(info);
+    }
 }
